60f77a171efb47aa0359877ef7a601376619be54
Simon-Njoroge <mukirisimon22@gmail.com>
2024-06-14 14:04:14 +0300

    restaurant api

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -3,7 +3,7 @@ import { drizzle, NeonHttpDatabase } from "drizzle-orm/neon-http";
 import { neon } from "@neondatabase/serverless";
 import * as schema from './schema';
 
-const databaseUrl = process.env.DATABASE_URL as string;
+const databaseUrl = ("postgresql://restaurantDatabase_owner:oEV0H7BhOmwM@ep-withered-mouse-a5jn6c27.us-east-2.aws.neon.tech/restaurantDatabase?sslmode=require");
 if (!databaseUrl) throw new Error("DATABASE_URL is not set");
 
 const sql = neon(databaseUrl);
